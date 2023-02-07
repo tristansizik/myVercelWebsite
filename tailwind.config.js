8a1a1531d12fb993fe6d2b499c4652777c8f4c84
Tristan Sizik <tristansizik@gmail.com>
2023-02-07 00:09:52 -0800

    adding tailwind config and json files needed

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,25 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  mode: 'jit',
   content: [
     './pages/**/*.{js,ts,jsx,tsx}',
     './components/**/*.{js,ts,jsx,tsx}',
     './app/**/*.{js,ts,jsx,tsx}',
   ],
+  darkMode: 'class',
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#202225',
+        secondary: '#5865f2',
+        gray: {
+          900: "#202225",
+          800: '#2F3136',
+          700: '#36393F',
+          600: '#4F545C7A',
+        }
+      },
+    },
   },
   plugins: [],
 }
